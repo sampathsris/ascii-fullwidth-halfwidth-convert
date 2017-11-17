53a20369962359f8d38813ea028107a6524c281f
Sampath Sitinamaluwa <sampathsris@gmail.com>
2017-11-17 18:25:33 +0530

    Implemented the functionality and added more tests.

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/*
+ * Full-width ASCII variants range from 0xFF01 to 0xFF5E.
+ * Therefore the non-inclusive upper and lower bounds for
+ * this range is 0xFF00 and 0xFF5F.
+ * 
+ * 0x3000 is the Ideographic Space, which is the full-width
+ * equivalent of space (0x0020) in ASCII.
+ */
+
+const toFullWidth = (charCode) => {
+    if (0x0020 < charCode && charCode < 0x007F) {
+        return 0xFF00 + (charCode - 0x0020);
+    }
+
+    if (0x0020 === charCode) {
+        return 0x3000;
+    }
+
+    return charCode;
+};
+
+const toHalfWidth = (charCode) => {
+    if (0xFF00 < charCode && charCode < 0xFF5F) {
+        return 0x0020 + (charCode - 0xFF00);
+    }
+
+    if (0x3000 === charCode) {
+        return 0x0020;
+    }
+
+    return charCode;
+};
+
+const process = (str, charProcess) => {
+    let ret = [];
+
+    for (let char of str) {
+        ret.push(charProcess(char.charCodeAt()));
+    }
+
+    return String.fromCharCode.apply(String, ret);
+};
+
+const factory = (charProcessor) => {
+    return (str) => {
+        return process(str, charProcessor);
+    }
+}
+
+module.exports = {
+    toFullWidth: factory(toFullWidth),
+    toHalfWidth: factory(toHalfWidth)
+};
--- test.js
+++ test.js
@@ -1,38 +1,65 @@
 
 describe('Testing ascii-fullwidth-haflwidth-convert', () => {
     const afhc = require('.');
+    
     const HALF_WIDTH =
         '!"#$%&\'()*+,-./0123456789:;<=>?' +
         '@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_' +
-        '`abcdefghijklmnopqrstuvwxyz{|}~';
+        '`abcdefghijklmnopqrstuvwxyz{|}~' +
+        '\u0020';
     const FULL_WIDTH =
         '！＂＃＄％＆＇（）＊＋，－．／' +
         '０１２３４５６７８９：；＜＝＞？' +
         '＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯ' +
         'ＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿' +
         '｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏ' +
-        'ｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～';
+        'ｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～' +
+        '\u3000';
     const NUM_CODE_POINTS = HALF_WIDTH.length;
+    
+    const MIXED = 'THE ｑｕｉｃｋ， ＢＲＯＷＮ\u3000fox.';
+    const MIXED_TO_FULL = 'ＴＨＥ\u3000ｑｕｉｃｋ，\u3000ＢＲＯＷＮ\u3000ｆｏｘ．';
+    const MIXED_TO_HALF = 'THE quick, BROWN fox.'
 
     describe('Testing toFullWidth', () => {
         for (let i = 0; i < NUM_CODE_POINTS; i++) {
             let halfWidthChar = HALF_WIDTH[i];
             let fullWidthChar = FULL_WIDTH[i];
 
-            it ('Should convert half-width ' + halfWidthChar + ' to full-width ' + fullWidthChar + '.', () => {
+            it ('Should convert ' + halfWidthChar + ' to ' + fullWidthChar + '.', () => {
                 afhc.toFullWidth(halfWidthChar).should.be.exactly(fullWidthChar);
             });
         }
+
+        it('Should convert the half-width characters to full-width characters.', () => {
+            afhc.toFullWidth(HALF_WIDTH).should.be.exactly(FULL_WIDTH);
+        });
+
+        it('Should convert mixed string to full width', () => {
+            afhc.toFullWidth(MIXED).should.be.exactly(MIXED_TO_FULL);
+        });
     });
 
-    describe('Testing fromFullWidth', () => {
+    it('Should convert the half-width characters to full-width characters.', () => {
+        afhc.toHalfWidth(FULL_WIDTH).should.be.exactly(HALF_WIDTH);
+    });
+
+    describe('Testing toHalfWidth', () => {
         for (let i = 0; i < NUM_CODE_POINTS; i++) {
             let halfWidthChar = HALF_WIDTH[i];
             let fullWidthChar = FULL_WIDTH[i];
 
-            it ('Should convert full-width ' + fullWidthChar + ' to half-width ' + halfWidthChar + '.', () => {
-                afhc.toFullWidth(fullWidthChar).should.be.exactly(halfWidthChar);
+            it ('Should convert ' + fullWidthChar + ' to ' + halfWidthChar + '.', () => {
+                afhc.toHalfWidth(fullWidthChar).should.be.exactly(halfWidthChar);
             });
         }
+        
+        it('Should convert the full-width characters to half-width characters.', () => {
+            afhc.toHalfWidth(FULL_WIDTH).should.be.exactly(HALF_WIDTH);
+        });
+
+        it('Should convert mixed string to half width', () => {
+            afhc.toHalfWidth(MIXED).should.be.exactly(MIXED_TO_HALF);
+        });
     });
 });
